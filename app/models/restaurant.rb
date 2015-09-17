c7ece71072e8e88d025c080be025fcebaf5a4ba5
Romain BERNARD <romain.bernard@edhec.com>
2015-09-17 15:39:12 +0200

    fixing gst

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -14,4 +14,11 @@
 
 class Restaurant < ActiveRecord::Base
   has_many :reviews, dependent: :destroy
+
+  geocoded_by :address
+  after_validation :geocode, if: :address_changed?
+
+  validates :name, presence: true
+  validates :address, presence: true
+
 end
